d86bcbc95e1469fe704dbe5c6ff82715bc7f01a4
Quentin Couland <Coulis1990@gmail.com>
2024-06-21 00:28:00 +0200

    :art: Reflected changes to the main function

--- oboyrog/main.c
+++ oboyrog/main.c
@@ -1,15 +1,22 @@
-#include <ncurses.h>
+#include "include_ncurses.h"
 #include "entity.h"
+#include "dungeon/dungeon.h"
 #include "input_handler.h"
 #include "renderer.h"
+
 
 int main(void) {
 
     init_ncurses_rendering();
 
-    char input;
+    char input = ',';
 
     Entity player = {{10, 20}, '@', 15};
+    Dungeon* dungeon = create_empty_dungeon();
+    //draw_debug_grid(dungeon, 5);
+    draw_room(dungeon, 10, 10, 20, 10);
+
+
 
     while (input != 'a') {
         // Get inputted char with needing to validate with ENTER
@@ -18,15 +25,17 @@ int main(void) {
         // Handle user input
         handle_input(input, &player);
 
+        mvaddch(10, 10, input);
+
         // Copy void character to the whole console
         clear_console();
 
+        render_dungeon(dungeon);
         // Display player
         render_entity(&player);
     }
 
-
-    endwin();
+    close_ncurses_rendering();
 
     return 0;
 }
