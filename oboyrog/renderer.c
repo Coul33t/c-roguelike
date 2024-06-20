5dfeab2ad1a9f1f0c8ffb33b2b40ca77d64e7fc0
Quentin Couland <Coulis1990@gmail.com>
2024-06-21 00:24:19 +0200

    :sparkles: Added dungeon rendering

--- oboyrog/renderer.c
+++ oboyrog/renderer.c
@@ -15,10 +15,18 @@ void clear_console() {
     clear();
 }
 
+void close_ncurses_rendering() {
+    endwin();
+}
+
 void render_entity(Entity* entity) {
     mvaddch(entity->pos.y, entity->pos.x, entity->chr);
 }
 
 void render_dungeon(Dungeon* dungeon) {
-
+    for (int x = 0; x < DUNGEON_SIZE_X; x++) {
+        for (int y = 0; y < DUNGEON_SIZE_Y; y++) {
+            mvaddch(x, y, dungeon->map[(x * DUNGEON_SIZE_Y) + y]->chr);
+        }
+    }
 }
--- oboyrog/renderer.h
+++ oboyrog/renderer.h
@@ -1,12 +1,13 @@
 #ifndef __RENDERER_H__
 #define __RENDERER_H__
 
-#include <ncurses.h>
+#include "include_ncurses.h"
 #include "entity.h"
 #include "dungeon/dungeon.h"
 
 void init_ncurses_rendering();
 void clear_console();
+void close_ncurses_rendering();
 
 void render_entity(Entity* entity);
 void render_dungeon(Dungeon* dungeon);
