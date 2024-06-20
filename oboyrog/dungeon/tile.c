2d5ceb7feb9c4b726284d6e1a48f84c18898a7b3
Quentin Couland <Coulis1990@gmail.com>
2024-06-21 00:22:31 +0200

    :sparkles: Added new tiles + corrected errors

--- oboyrog/dungeon/tile.c
+++ oboyrog/dungeon/tile.c
@@ -1,16 +1,55 @@
 #include "tile.h"
 
+Tile* get_tile(TILE_TYPE tile_type) {
+
+}
+
+Tile* get_void() {
+    Tile* tile = calloc(sizeof(Tile), 1);
+    tile->chr = ' ';
+    tile->walkable = false;
+    tile->transparent = false;
+    tile->visible = false;
+    tile->seen = false;
+    return tile;
+}
+
+Tile* get_debug() {
+    Tile* tile = calloc(sizeof(Tile), 1);
+    tile->chr = 'D';
+    tile->walkable = false;
+    tile->transparent = false;
+    tile->visible = false;
+    tile->seen = false;
+    return tile;
+}
+
 Tile* get_wall() {
-    Tile* tile = {'#', false, false, false, false};
+    Tile* tile = calloc(sizeof(Tile), 1);
+    tile->chr = '#';
+    tile->walkable = false;
+    tile->transparent = false;
+    tile->visible = false;
+    tile->seen = false;
     return tile;
 }
 
 Tile* get_floor() {
-    Tile* tile = {'.', true, true, false, false};
+    Tile* tile = calloc(sizeof(Tile), 1);
+    tile->chr = '.';
+    tile->walkable = true;
+    tile->transparent = true;
+    tile->visible = false;
+    tile->seen = false;
     return tile;
 }
 
 Tile* get_glass() {
-    Tile* tile = {'=', false, true, false, false};
+    Tile* tile = calloc(sizeof(Tile), 1);
+    tile->chr = '=';
+    tile->walkable = false;
+    tile->transparent = true;
+    tile->visible = false;
+    tile->seen = false;
     return tile;
 }
--- oboyrog/dungeon/tile.h
+++ oboyrog/dungeon/tile.h
@@ -1,7 +1,14 @@
 #ifndef __TILE_H__
 #define __TILE_H__
 
+#include <stdlib.h>
 #include <stdbool.h>
+
+typedef enum {
+    WALL,
+    FLOOR,
+    GLASS
+} TILE_TYPE;
 
 typedef struct Tile {
     char chr;
@@ -11,6 +18,9 @@ typedef struct Tile {
     bool seen;
 } Tile;
 
+Tile* get_tile(TILE_TYPE tile_type);
+Tile* get_debug();
+Tile* get_void();
 Tile* get_wall();
 Tile* get_floor();
 Tile* get_glass();
