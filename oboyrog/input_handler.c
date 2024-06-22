955856e63e1ac2b19a67f0f846e1fd26b3bc2276
Quentin Couland <Coulis1990@gmail.com>
2024-06-22 12:51:52 +0200

    :sparkles: Now checks if move is valid before moving

--- oboyrog/input_handler.c
+++ oboyrog/input_handler.c
@@ -1,19 +1,25 @@
 #include "input_handler.h"
 
-void handle_input(char input, Entity* player) {
+void handle_input(char input, Entity* player, Dungeon* dungeon) {
     if (input == 'z') {
-        move_entity(player, NORTH);
+        move_if_valid(player, NORTH, dungeon);
     }
 
     else if (input == 's') {
-        move_entity(player, SOUTH);
+        move_if_valid(player, SOUTH, dungeon);
     }
 
     else if (input == 'd') {
-        move_entity(player, EAST);
+        move_if_valid(player, EAST, dungeon);
     }
 
     else if (input == 'q') {
-        move_entity(player, WEST);
+        move_if_valid(player, WEST, dungeon);
+    }
+}
+
+void move_if_valid(Entity* entity, DIRECTION dir, Dungeon* dungeon) {
+    if (get_dungeon_tile_from_dir(dungeon, dir, entity->pos.x, entity->pos.y)->walkable) {
+        move_entity(entity, dir);
     }
 }
--- oboyrog/input_handler.h
+++ oboyrog/input_handler.h
@@ -1,8 +1,15 @@
 #ifndef __INPUT_HANDLER_H__
 #define __INPUT_HANDLER_H__
 
+#include <stdbool.h>
+
+#include "dungeon/dungeon.h"
 #include "entity.h"
 
-void handle_input(char input, Entity* player);
+#include "comp/position.h"
+
+void handle_input(char input, Entity* player, Dungeon* dungeon);
+
+void move_if_valid(Entity* entity, DIRECTION dir, Dungeon* dungeon);
 
 #endif
