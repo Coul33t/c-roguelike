66547f77e534c2fe01bb411c3272e12006a3252d
Quentin Couland <couland-q@ird222-07-l.etu.polytech.univ-nantes.prive>
2024-06-26 15:42:00 +0200

    :memo: Added a few constants

--- oboyrog/constants.h
+++ oboyrog/constants.h
@@ -1,14 +1,30 @@
 #ifndef __CONSTANTS_H__
 #define __CONSTANTS_H__
 
+// -------------------
+// ----- COLOURS -----
+// -------------------
 #define DEFAULT_COLOURS 1
 #define VISIBLE_COLOURS 2
 #define SEEN_COLOURS 3
+
+#define RED_FG_COLOUR 4
+#define RED_BG_COLOUR 5
+
+#define YELLOW_FG_COLOUR 6
+#define YELLOW_BG_COLOUR 7
+// -------------------
+
 
 #define CONSOLE_X_SIZE 80
 #define CONSOLE_Y_SIZE 24
 
 #define DUNGEON_SIZE_X 100
 #define DUNGEON_SIZE_Y 100
+
+
+#define BASE_FOV 15
+
+#define NB_MAX_MONSTERS 100
 
 #endif
