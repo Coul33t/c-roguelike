5db6fa3845c756240514f146681b309c3948b033
Quentin Couland <Coulis1990@gmail.com>
2024-07-02 17:40:52 +0200

    :construction: Starting message display

---
+++ oboyrog/message.c
@@ -0,0 +1,5 @@
+#include "message.h"
+
+void display_message(char* message, int x, int y) {
+
+}
---
+++ oboyrog/message.h
@@ -0,0 +1,6 @@
+#ifndef __MESSAGE_H__
+#define __MESSAGE_H__
+
+void display_message(char* message, int x, int y);
+
+#endif
